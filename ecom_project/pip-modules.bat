53e37b89fd0f7438c1c8c1ebb129c717a62d1273
DhuJUICE <junierherandien84@gmail.com>
2024-11-25 16:41:19 +0200

    added requirements

---
+++ ecom_project/pip-modules.bat
@@ -0,0 +1,10 @@
+pip install djangorestframework-simplejwt
+pip install django
+pip install psycopg2-binary
+pip install pillow
+pip install djangorestframework
+pip install requests
+pip install gunicorn
+pip install django-cors-headers
+pip install drf-yasg
+pip install --upgrade setuptools
