d44efb7ababa547c4a76efe8a6cd3cef8c658128
Ayanda2002 <133391807+Ayanda2002@users.noreply.github.com>
2024-12-06 15:43:26 +0200

    Updated user management views & urls

--- ecom_project/user_management/urls.py
+++ ecom_project/user_management/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-	path('user', views.DisplayPage)
+	path('sign', views.SignUpPage),
+	path('login', views.LoginPage)
 ]
--- ecom_project/user_management/views.py
+++ ecom_project/user_management/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-def DisplayPage(request):
+def LoginPage(request):
 	return render(request, 'login.html')
+
+def SignUpPage(request):
+	return render(request, 'sign_up.html')
