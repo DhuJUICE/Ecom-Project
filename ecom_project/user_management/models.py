93cfc3ca2c0003561bd4fc3ac2f2b91ddbfd70be
Ayanda2002 <133391807+Ayanda2002@users.noreply.github.com>
2024-12-07 16:53:24 +0200

    Added role field

--- ecom_project/user_management/models.py
+++ ecom_project/user_management/models.py
@@ -2,10 +2,21 @@ from django.db import models
 
 # Create your models here.
 class user(models.Model):
+    ROLE_CHOICES = [
+        ('customer', 'Customer'),
+        ('staff', 'Staff'),
+        ('admin', 'Admin'),
+    ]
+
     user_id = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=30)  
     last_name = models.CharField(max_length=30)   
     email_address = models.EmailField(unique=True)
+    role = models.CharField(
+        max_length=10, 
+        choices=ROLE_CHOICES, 
+        default='customer'
+    )
 
     def __str__(self):
-        return self.first_name + " " + self.last_name + " " + self.email_address
+        return self.first_name + " " + self.last_name + " " + self.email_address + " " + self.role
