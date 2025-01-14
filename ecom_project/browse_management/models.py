fff0146ede906a5d97821926c79802b175c66ff6
DhuJUICE <junierherandien84@gmail.com>
2025-01-14 23:35:54 +0200

    added MENU model
    Users can use the MENU model to see which products they want to add to their CART

--- ecom_project/browse_management/models.py
+++ ecom_project/browse_management/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from product_management.models import PRODUCT
 
 # Create your models here.
+class MENU(models.Model):
+
+	#menu for browsing products for cart
+	productId = models.ForeignKey(PRODUCT, on_delete=models.CASCADE)
+	
+	#track the quantity to buy of the specific product
+	itemQuantAdded = models.IntegerField()
+
+	#get the total of the product that was purchased(itemQuantAdded*productPrice)
+	itemTotal = models.DecimalField(max_digits=10, decimal_places=2)
