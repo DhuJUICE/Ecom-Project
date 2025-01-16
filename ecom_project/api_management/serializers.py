82517507a1b955c7651e6f5aafca8a77c33d48dc
DhuJUICE <junierherandien84@gmail.com>
2025-01-16 13:06:59 +0200

    Added serializers for all models
    This is to convert django models into JSON serializable objects, and vice versa

---
+++ ecom_project/api_management/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from product_management.models import PRODUCT
+from browse_management.models import MENU
+from cart_management.models import CART
+from checkout_management.models import TRANSACTION_LOG
+from django.contrib.auth.models import User
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PRODUCT
+        fields = '__all__'
+
+class MenuSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MENU
+        fields = '__all__'
+
+class CartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CART
+        fields = '__all__'
+
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PRODUCT
+        fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PRODUCT
+        fields = '__all__'
