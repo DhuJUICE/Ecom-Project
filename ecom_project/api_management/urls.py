28b443d48d6788e5cfd45ed3bf5ae0c40ca82c6f
DhuJUICE <junierherandien84@gmail.com>
2025-01-20 19:41:31 +0200

    added token endpoints for getting and refreshing auth tokens

--- ecom_project/api_management/urls.py
+++ ecom_project/api_management/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 from . import views
+
+#token view imports
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 urlpatterns = [
-	path('api', views.DisplayPage)
+	path('api', views.DisplayPage),
+	path('api/token', TokenObtainPairView.as_view(), name='token'),
+    path('api/token/refresh', TokenRefreshView.as_view(), name='token-refresh'),
 ]
